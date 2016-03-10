8333a2c5a9032ab0aa0618dea2c9fa4facac6e16
ProtossArtanis <steinsgate@mac24.local>
2016-03-10 10:46:35 +0800

    Initial Commit

---
+++ day02/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  day02
+//
+//  Created by SteinsGate on 16/3/10.
+//  Copyright © 2016年 SteinsGate. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
